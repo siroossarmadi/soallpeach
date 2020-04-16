9f2a9a52f3f8e2b585decd41181757e26f81ded9
siroossarmadi <siroos.sarmadi@gmail.com>
2020-04-16 08:16:16 +0430

    Add goroutine and output change

--- prime/main.go
+++ prime/main.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"fmt"
 	"io/ioutil"
 	"os"
 	"strconv"
@@ -10,14 +9,12 @@ import (
 
 var mark = make([]bool, 1000000)
 var primes = make([]int, 78498)
-var ans = make([]string, 0)
+var lines []string
 
-func main() {
-	path := os.Args[1]
+func sieve(ex chan string) {
 	mark[0] = true
 	mark[1] = true
 	p := 0
-	//
 	for i := 2; i < 1000000; i++ {
 		if mark[i] {
 			continue
@@ -28,13 +25,28 @@ func main() {
 		primes[p] = i
 		p++
 	}
+	ex <- "ex"
+}
 
+func read(path string, end chan string) {
 	b, err := ioutil.ReadFile(path)
 	if err != nil {
 		panic(err)
 	}
+	lines = strings.Split(string(b), "\n")
 
-	lines := strings.Split(string(b), "\n")
+	end <- "ex"
+}
+
+func main() {
+	path := os.Args[1]
+	ex := make(chan string, 2)
+	go read(path, ex)
+	go sieve(ex)
+	<-ex
+	<-ex
+	var ans = make([]byte, len(lines)*2)
+	j := 0
 	for _, l := range lines {
 		if len(l) == 0 {
 			continue
@@ -46,23 +58,26 @@ func main() {
 		if n >= 1000000 {
 			for _, p := range primes {
 				if p >= 46340 || p*p > n {
-					ans = append(ans, "1")
+					ans[j] = 49
+					ans[j+1] = 10
 					break
 				} else if n%p == 0 {
-					ans = append(ans, "0")
+					ans = append(ans, 48)
+					ans = append(ans, 10)
 					break
 				}
 			}
 		} else {
 			if mark[n] {
-				ans = append(ans, "0")
+				ans[j] = 48
+				ans[j+1] = 10
 			} else {
-				ans = append(ans, "1")
+				ans[j] = 49
+				ans[j+1] = 10
 			}
 		}
-
+		j += 2
 	}
+	os.Stdout.Write(ans)
 
-	str := strings.Join(ans, "\n")
-	fmt.Print(str)
 }
